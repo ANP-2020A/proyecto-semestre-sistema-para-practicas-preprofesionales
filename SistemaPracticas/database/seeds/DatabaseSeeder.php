7167b3fb96f8215059802803f6781bc91ca13024
Cristian Guamba <cristiangpbf@gmail.com>
2020-08-04 10:08:52 -0500

    model

--- SistemaPracticas/database/seeds/DatabaseSeeder.php
+++ SistemaPracticas/database/seeds/DatabaseSeeder.php
@@ -8,10 +8,11 @@ class DatabaseSeeder extends Seeder
     {
         Schema::disableForeignKeyConstraints();
         $this->call(UsersTableSeeder::class);
-        $this->call(EmpresasTableSeeder::class);
         $this->call(CapacitacionsTableSeeder::class);
         $this->call(CarrerasTableSeeder::class);
+        $this->call(EmpresasTableSeeder::class);
         $this->call(EstudiantePostulacionTableSeeder::class);
+
         $this->call(HabilidadsTableSeeder::class);
         $this->call(InstruccionsTableSeeder::class);
         $this->call(OfertasTableSeeder::class);
