fcceb52a6b5aef4008da40cde66265d0c0d68fb6
Cristian Guamba <cristiangpbf@gmail.com>
2020-09-05 17:18:56 -0500

    politicas para job

--- SistemaPracticas/app/Http/Controllers/JobController.php
+++ SistemaPracticas/app/Http/Controllers/JobController.php
@@ -10,20 +10,30 @@ use Illuminate\Http\Request;
 class JobController extends Controller
 {
     public function index(){
+        $this->authorize('viewAny', Job::class);
+
         return new JobCollection(Job::paginate());
     }
     public function show(Job $job){
+        $this->authorize('view', $job);
+
         return response()->json(new JobResource($job),200);
     }
     public function store(Request $request){
+        $this->authorize('create', Job::class);
+
         $job= Job::create($request->all());
         return response()->json($job,201);
     }
     public function update(Request $request,Job $job){
+        $this->authorize('update', $job);
+
         $job->update($request->all());
         return response()->json($job,200);
     }
     public function delete(Job $job){
+        $this->authorize('delete', $job);
+
         $job->delete();
         return response()->json(null,204);
     }
